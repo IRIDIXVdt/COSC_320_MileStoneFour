0e9f10cb770162c03039f19ad5a04c3d98e14e95
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2021-04-05 12:22:18 +0800

    Update graph.py

--- graph.py
+++ graph.py
@@ -20,9 +20,6 @@ import math
         print (cell_array)
     cell_array2 = [list(map (int,i)) for i in cell_array]
 
-# 你的graph的确能创建一个随机的map 坐标都在范围之内 很好 但是我有两个疑问
-# 1. 为什么一定需要输出 然后读取一个文件 我不太明白这一步的必要性
-# 1.1 写了一点code之后我意识到了更严重的问题  这个把数据都按照String 存了 再变回int 真的效率不算高 请你赶紧修改一下
 
 # （2. 这个先不着急 但是这个随机的分布 真的没问题么 我觉得按照一个模拟现实的地图 
     # 中间会更加密集 四周会疏散 你现在的地图多半每个点出现城市的几率是相同的）
@@ -60,6 +57,20 @@ removePair(distanceMap,2,3)
 
 print(distanceMap)
 
+def randomFlightPlan( map, branchingFactor, qty):
+    # we first create a copy of the array
+    
+    # notice that the original branching factor would be qty-1
+    chance = branchingFactor/(qty-1)
+    print(chance)
+    for i in range(0,qty):
+        for j in range(i,qty):
+            target = random.random()
+            if (target<chance):
+                removePair(map,i,j)
+
+
+randomFlightPla
 
 
     

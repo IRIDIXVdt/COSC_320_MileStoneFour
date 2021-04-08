a469f72b2a2c4032261452f975101390699cc7c5
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2021-04-08 14:09:11 +0800

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,9 +1,9 @@
 
 
-def a_star_search_algorithm(graph,heuristic,start=0,end):
-    frontier = {}
+def a_star_search_algorithm(graph=[[]], heuristic=[[]], start_node=0, end_node):
+    frontier = [start_node]
     # to store the list of nodes we are about to pop
-    explored = {}
+    explored = [start_node]
     # to store the list of nodes we have explored
     while true:
         if(len(frontier) == 0):
@@ -11,12 +11,22 @@ def a_star_search_algorithm(graph,heuristic,start=0,end):
             # as there is no solution
         current = frontier.pop(0)
         # this is the node of the lowest cost
-        if(current == end):
+        if(current == end_node):
             print('found solution')
-            return end
+            return end_node
         # we have the optimal solution
         explored.append(current)
-        
+        # add the state of node to the expolored list
+        current_node_neighbours = find_neighbours(graph,current)
+        for child in current_node_neighbours:
+            # now we iterate through all available actions in node
+            
+            # define child as the successor we are looking at in the iteratoipn
+            if(explored.index(child) == -1 && frontier.index(child) == -1):
+                frontier.append(child)
+                frontier.sort()
+                # if child's state is not in frontier or explored then we add it to the frontier
+            else if():
     return None
 
 

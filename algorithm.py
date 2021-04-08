89677a78ee4b2cc76948c5cb0864656f87052b4e
yihewang0506 <77508297+yihewang0506@users.noreply.github.com>
2021-04-07 22:53:06 -0700

    neighbour4

--- algorithm.py
+++ algorithm.py
@@ -7,13 +7,11 @@ def a_star_search_algorithm(graph,heuristic,start=0,end):
     return None
 
 def find_neighbours(graph,current_node):
-    for i in range(len(graph)):
-        for j in range(len(graph[i])):
-            if graph[i][j] == 0:
-                current_node = graph[i][j]
-            if graph[i][j] > 0:
-                
+    a = graph[current_node]
+    neighbours = []
+    for i in range (len(a)):
+        if a[i] > 0:
+            append.neighbours(i)
 
-    neighbours = {}
 
     return neighbours

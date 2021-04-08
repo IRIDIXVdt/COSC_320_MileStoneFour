22757b83793a35c112975fd8ba8c4a814ff19494
yihewang0506 <77508297+yihewang0506@users.noreply.github.com>
2021-04-07 22:15:41 -0700

    A_star_search_algorithm

--- A_star_search.py
+++ A_star_search.py
@@ -1,89 +1,14 @@
 from queue import PriorityQueue
+from graph2 import Graph
 class a_star_search:
-    def __init__(slef, name)
-    def a_star(self,g, start, goal):
-        frontier = PriorityQueue()
-        frontier.put(start, 0)
-        came_from = {start: None}
-        cost_so_far = {start: 0}
-        visted = []
-        while not frontier.empty():
-            current = frontier.get()
-            visted.append(current)
-            if goal == current:
-                break
-            for nbr in g.get_vertex(current).get_connections():
-                print(current, nbr)
-                new_cost = cost_so_far[current] + g.get_vertex(current).get_weight(nbr)
-                if nbr.get_id() not in cost_so_far.keys() or new_cost < cost_so_far[nbr.get_id()]:
-                    cost_so_far[nbr.get_id()] = new_cost
-                # priority = new_cost + heuristic(goal, nbr)
-                    priority = new_cost
-                    frontier.put(nbr.get_id(), priority)
-                    came_from[nbr.get_id()] = current
-        print("cost_so_far", cost_so_far, cost_so_far.keys())
-        print("came_from", came_from)
-
-        res = [start]
-        while True:
-            find = False
-            for k, v in came_from.items():
-            # print(k, v)
-                if res[-1] == v:
-                    res.append(k)
-                    find = True
-            if find:
-                continue
-            if res[-1] == goal:
-                break
-            else:
-                came_from.pop(res[-1])
-                res.pop(-1)
-
-        print(res)
-    # return res
+   def __init__(self,):
 
 
-    def a_star_algorithm(self,g, start, goal):
-        open_lst = set([start])
-        closed_lst = set([])
-        poo = {start: 0}
-        par = {start: start}
-        while len(open_lst) > 0:
-            n = None
-            for v in open_lst:
-                if n is None or poo[v] < poo[n]:
-                    n = v
-            if n is None:
-                print('Path does not exist!')
-                return None
 
-            if n == goal:
-                reconst_path = []
-                while par[n] != n:
-                    reconst_path.append(n)
-                    n = par[n]
-                reconst_path.append(start)
-                reconst_path.reverse()
-                print('Path found: {}'.format(reconst_path))
-                return reconst_path
+   def heuristic():
 
-            for nbr in g.get_vertex(n).get_connections():
+   
+   
+   def a_star_search_algorithm(graph,heuristic,star,end):
 
-                if nbr.get_id() not in open_lst and nbr.get_id() not in closed_lst:
-                    open_lst.add(nbr.get_id())
-                    par[nbr.get_id()] = n
-                    poo[nbr.get_id()] = poo[n] + g.get_vertex(n).get_weight(nbr)
-                else:
-                    if poo[nbr.get_id()] > poo[n] + g.get_vertex(n).get_weight(nbr):
-                        poo[nbr.get_id()] = poo[n] + g.get_vertex(n).get_weight(nbr)
-                        par[nbr.get_id()] = n
 
-                        if nbr.get_id() in closed_lst:
-                            closed_lst.remove(nbr.get_id())
-                            open_lst.add(nbr.get_id())
-
-            open_lst.remove(n)
-            closed_lst.add(n)
-
-        return None
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import random
 from queue import PriorityQueue
 from graph2 import Graph
-
+from A_star_search import a_star_search
 
 #def heuristic(goal, nbr):
     #return abs(goal - nbr.get_id())
@@ -22,17 +22,14 @@ if __name__ == "__main__":
     print(flightPlanMatrix)
     #兄弟今天我没法跟你meeting了，我这边360的队友们找我meeting写code
     #关于nodes的想法：找到每个key。我感觉用matrix的index就可行 然后把哪个点与哪个点之间有连线 类似于1->2,3 2->5这种我没想到怎么实现它或者我整个的思路就有问题
-    # https://www.annytab.com/a-star-search-algorithm-in-python/ 这个是我在网上找的例子
+    #https://www.annytab.com/a-star-search-algorithm-in-python/ 这个是我在网上找的例子
     #
-    for i in range(len(flightPlanMatrix)):
-        for j in range(len(flightPlanMatrix[i])):
-            if flightPlanMatrix[i][j] == 0:
-                a = j+1
-                print(a)
-            if flightPlanMatrix[i][j] > 0:
-                b = j+1
-                print(b)
+    
     g.calBF(flightPlanMatrix)
+    search = a_star_search()
+    heuristic = search.heuristic()
+    find_path = search.a_star_search_algorithm()
+
 
     
     

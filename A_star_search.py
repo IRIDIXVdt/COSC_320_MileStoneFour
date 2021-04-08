3465d2f506caf6771f7d1c18a85d91e2ec518ec9
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2021-04-08 13:31:04 +0800

    Update A_star_search.py

--- A_star_search.py
+++ A_star_search.py
@@ -2,12 +2,7 @@ from queue import PriorityQueue
 from graph2 import Graph
 class a_star_search:
    def __init__(self,):
-
-
-
-   def heuristic():
-
-   
+       
    
    def a_star_search_algorithm(graph,heuristic,star,end):
 

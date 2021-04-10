f756c21032e3d9fb8beb526cd20ba39001dbe45d
yihewang0506 <77508297+yihewang0506@users.noreply.github.com>
2021-04-09 20:59:25 -0700

    PlotRunningTime2

--- PlotRunningTime.py
+++ PlotRunningTime.py
@@ -1,8 +1,8 @@
 from graph2 import Graph
-import algorithm 
 import time
 import random
-def runningTime(graph, Ns = range(1,5000, 500), numTrials = 20):
+import algorithm
+def runningTime(graph, Ns = range(1,5000, 500), numTrials = 5):
     #Ns X-axis start form 0, end at 5000, 500 incremental 
     #numTrials At one point, run numTrials times then get the average time
     nValues = []	
@@ -10,14 +10,16 @@ def runningTime(graph, Ns = range(1,5000, 500), numTrials = 20):
     for n in Ns:
         runtime = 0
         for i in range(numTrials):
-            #g = Graph(100,100,100)
-            #mapFlight = g.read_graph()   # read map
-            #distanceMatrix = g.generateMatrix()
-            #flightPlanMatrix = g.randomFlightPlan(distanceMatrix,6)    # create flight plan matrix
-            #startPoint = random.randint(0,99)
-            #endPoint = random.randint(0,99)
+
+            g = Graph(5000,5000,n) # We 'Draw' a map in a coordinate system
+            mapFlight = g.read_graph()   # read map
+            distanceMatrix = g.generateMatrix()    # create distance matrix
+            flightPlanMatrix = g.randomFlightPlan(distanceMatrix,6)    # create flight plan matrix
+            startPoint = random.randint(0,n-1)
+            endPoint = random.randint(0,n-1)
             start = time.time()#start time
-            #algorithm.a_star_search(flightPlanMatrix,flightPlanMatrix,startPoint,endPoint)
+            algorithm.a_star_search(flightPlanMatrix,flightPlanMatrix,startPoint,endPoint) #call algorithm
+            algorithm.uniform_cost_search(flightPlanMatrix,startPoint,endPoint)
             end = time.time() #end time
             runtime += (end - start) * 1000 #cover to milliseconds
         runtime = runtime/numTrials #get the average
--- test.py
+++ test.py
@@ -8,23 +8,26 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    g = Graph(100,100,100) # We 'Draw' a map in a coordinate system
+    g = Graph(100,100,10) # We 'Draw' a map in a coordinate system
     mapFlight = g.read_graph()   # read map
     distanceMatrix = g.generateMatrix()    # create distance matrix
     flightPlanMatrix = g.randomFlightPlan(distanceMatrix,6)    # create flight plan matrix
     
     g.calBF(flightPlanMatrix)
 
-    test1 = algorithm.uniform_cost_search(flightPlanMatrix,5,10)
+    test1 = algorithm.uniform_cost_search(flightPlanMatrix,5,3)
     
-    test2 = algorithm.a_star_search(flightPlanMatrix,flightPlanMatrix,5,10)
+    test2 = algorithm.a_star_search(flightPlanMatrix,flightPlanMatrix,5,3)
     # print(test)
-    #ns = range(1,1000,50) #set X-axis range and incremental 
-    #nValues, tValues = PlotRunningTime.runningTime(g,Ns = ns, numTrials = 10)
-    #plt.plot(nValues, tValues, color="red", label="Random Graph Runing Time")
-    #plt.xlabel('n')
-    #plt.ylabel('Time(ms)')
-    #plt.title('Random Graph Test')
-    #plt.legend()
-    #plt.show()
+    ns = range(1,5000,500) #set X-axis range and incremental 
+    nValues, tValues = PlotRunningTime.runningTime(g,Ns = ns, numTrials = 10)
+    nValues2, tValues2 = PlotRunningTime.runningTime(g,Ns = ns, numTrials = 10)
+    plt.plot(nValues, tValues, color="red", label="A Star Search")
+    plt.plot(nValues2, tValues2, "--", color="blue", label="UCS")
+    plt.plot()
+    plt.xlabel('n')
+    plt.ylabel('Time(ms)')
+    plt.title('A star search running time')
+    plt.legend()
+    plt.show()
     

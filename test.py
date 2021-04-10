65a481b19a7e2819c87b035636ce7666ed889f12
yihewang0506 <77508297+yihewang0506@users.noreply.github.com>
2021-04-09 21:54:31 -0700

    Plot3

--- test.py
+++ test.py
@@ -21,14 +21,14 @@ if __name__ == "__main__":
     # print(test)
     ns = range(1,100,20) #set X-axis range and incremental 
     factor = range(1,10,1)
-    nValues, tValues = PlotRunningTime.runningTime(g,Ns = ns, numTrials = 10)
-    nValues2, tValues2 = PlotRunningTime.runningTime(g,Ns = ns, numTrials = 10)
-    #plt.plot(nValues, tValues, color="red", label="A Star Search")
-    #plt.plot(nValues2, tValues2, "--", color="blue", label="UCS")
-    plt.plot(ns,[pow(6,b) for b in ns])
-    plt.xlabel('n')
-    plt.ylabel('Time(ms)')
-    plt.title('A star search running time')
+    nValues, tValues = PlotRunningTime.runningTime(g,Ns = ns, numTrials = 10) #put value in to the runningTime method in PlotRunningTime page
+    nValues2, tValues2 = PlotRunningTime.runningTime(g,Ns = ns, numTrials = 10)#put value in to the runningTime method in PlotRunningTime page
+    plt.plot(nValues, tValues, color="red", label="A Star Search") #plot a star search running time with red line and a star search lable
+    plt.plot(nValues2, tValues2, "--", color="blue", label="UCS") #plot UCS running time with -- line, blue color and UCS lable
+    plt.plot(ns,[pow(6,b) for b in ns],color = "orange",label="O(d^b)") #plot O(d^b) running time with orange color and O(d^b) lable
+    plt.xlabel('n') #X represents n
+    plt.ylabel('Time(ms)')#Y represents Time(ms)
+    plt.title('A star search running time')#Graph title represents A star search running time
     plt.legend()
     plt.show()
     

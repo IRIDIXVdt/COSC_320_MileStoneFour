9bf04b84270799f87007d699b5e44333e9d5dcb6
IRIDIXVdt <61093228+IRIDIXVdt@users.noreply.github.com>
2021-04-10 15:20:17 +0800

    Standardize Code

--- PlotRunningTime.py
+++ PlotRunningTime.py
@@ -1,28 +1,28 @@
-from graph2 import Graph
-import time
-import random
-import algorithm
-def runningTime(graph, Ns = range(5,10,1), numTrials = 5, branching_factor =6):
-    #Ns X-axis start form 0, end at 5000, 500 incremental 
-    #numTrials At one point, run numTrials times then get the average time
-    nValues = []	
-    tValues = []
-    for n in Ns:
-        runtime = 0
-        for i in range(numTrials):
+# from graph2 import Graph
+# import time
+# import random
+# import algorithm
+# def runningTime(graph, Ns = range(5,10,1), numTrials = 5, branching_factor):
+#     #Ns X-axis start form 0, end at 5000, 500 incremental 
+#     #numTrials At one point, run numTrials times then get the average time
+#     nValues = []	
+#     tValues = []
+#     for n in Ns:
+#         runtime = 0
+#         for i in range(numTrials):
 
-            g = Graph(100,100,n) # We 'Draw' a map in a coordinate system
-            mapFlight = g.read_graph()   # read map
-            distanceMatrix = g.generateMatrix()    # create distance matrix
-            flightPlanMatrix = g.randomFlightPlan(distanceMatrix,branching_factor)    # create flight plan matrix
-            startPoint = random.randint(0,n-1)
-            endPoint = random.randint(0,n-1)
-            start = time.time()#start time
-            algorithm.a_star_search(flightPlanMatrix,flightPlanMatrix,startPoint,endPoint) #call algorithm
-            end = time.time() #end time
-            runtime += (end - start) * 1000 #cover to milliseconds
-        runtime = runtime/numTrials #get the average
-        nValues.append(pow(branching_factor,n))	#store n in this array
-        tValues.append(runtime)#store time in this array
-    return nValues,tValues
+#             g = Graph(100,100,n) # We 'Draw' a map in a coordinate system
+#             mapFlight = g.read_graph()   # read map
+#             distanceMatrix = g.generateMatrix()    # create distance matrix
+#             flightPlanMatrix = g.randomFlightPlan(distanceMatrix,branching_factor)    # create flight plan matrix
+#             startPoint = random.randint(0,n-1)
+#             endPoint = random.randint(0,n-1)
+#             start = time.time()#start time
+#             algorithm.a_star_search(flightPlanMatrix,flightPlanMatrix,startPoint,endPoint) #call algorithm
+#             end = time.time() #end time
+#             runtime += (end - start) * 1000 #cover to milliseconds
+#         runtime = runtime/numTrials #get the average
+#         nValues.append(pow(branching_factor,n))	#store n in this array
+#         tValues.append(runtime)#store time in this array
+#     return nValues,tValues
             
--- b_pow_d.py
+++ b_pow_d.py
@@ -1,9 +1,10 @@
 import time
 import random
 import algorithm
+import matplotlib
+import matplotlib.pyplot as plt
 
 class Quantity:
-    # Initialize the class for states in UCS
     def __init__(self, b,d):
         self.b = b
         self.d = d
@@ -12,19 +13,14 @@ class Quantity:
     def __lt__(self, other): # Sort nodes
          return pow(self.b,self.d) < pow(other.b,other.d)
 
-def b_power_dRunningTime(b = range(30,50,1),d = range(5,10,1)): 
-    nValues = []	
-    tValues = []
+def create_random_bdlist(b,d):
     bdlist = []
     for branching_dummy in b:
         branching_factor = branching_dummy/10
         for state in d:
             bdlist.append(Quantity(branching_factor,state))
     bdlist.sort()
-    for bd in bdlist:
-        nValues.append(pow(bd.b,bd.d))	#store n in this array
-        tValues.append(b_power_d_cal(bd.b,bd.d))#store time in this array
-    return nValues,tValues
+    return bdlist
 
 def b_power_d_cal(b,d):
     start = time.time()
@@ -35,3 +31,28 @@ def b_power_d_cal(b,d):
     end = time.time() #end time
     return (end - start) * 1000
 
+def b_power_dRunningTime(b,d): 
+    nValues = []	
+    tValues = []
+    for bd in create_random_bdlist(b,d):
+        nValues.append(pow(bd.b,bd.d))	#store n in this array
+        tValues.append(b_power_d_cal(bd.b,bd.d))#store time in this array
+    return nValues,tValues
+
+
+
+
+def show_all_time(distanceMatrix,flightPlanMatrix,b = range(30,50,1),d = range(5,10,1)):
+
+    nValues3, tValues3 = b_power_dRunningTime(b,d) #put value in to the b_power_dRunningTime in b_pow_d page
+  
+        # plt.plot(nValues, tValues, color="red", label="A Star Search") #plot a star search running time with red line and a star search lable
+    # plt.plot(nValues2, tValues2, "--", color="blue", label="UCS") #plot UCS running time with -- line, blue color and UCS lable
+    plt.plot(nValues3,tValues3,color = "orange" , label = "b_pow_d")
+    
+    
+    plt.xlabel('b^d') #X represents n
+    plt.ylabel('Time(ms)')#Y represents Time(ms)
+    plt.title('A star search running time')#Graph title represents A star search running time
+    plt.legend()
+    plt.show()
